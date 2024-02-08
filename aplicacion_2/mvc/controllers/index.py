2cda33a25b4e1d2d1bc7518b8bb1905991fe26a0
ArmandoCS23 CastelanSanchez <1722110105@utectulancingo.edu.mx>
2024-02-08 16:57:43 +0000

    paco

---
+++ aplicacion_2/mvc/controllers/index.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Mi Página de Prueba</title>
+</head>
+<body>
+
+    <h1>Hola, Mundo!</h1>
+    <p>Esta es mi primera página web de prueba. ¡Bienvenido!</p>
+
+</body>
+</html>
